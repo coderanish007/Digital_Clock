1ce61386df5ae5e286ef8d178f88d28f1870f805
AnishChettri <anishchettri112@gmail.com>
2022-07-26 21:23:36 +0530

    added views

--- date_and_time/Display/views.py
+++ date_and_time/Display/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+from datetime import datetime
 
 # Create your views here.
+def index(request):
+    
+    date = str(datetime.now().date())
+    time = str(datetime.now().time())
+    html = f'''
+    <html>
+        <body>
+            <h1>Current Date and Time is: </h1>
+            <h3>Date: {date}</h3>
+            <h3>Time: {time}</h3>
+        </body>
+    </html>
+    '''
+    return HttpResponse(html)
