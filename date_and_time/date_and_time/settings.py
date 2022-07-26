ffe938e30956ca9e095800c537c88d319b872423
AnishChettri <anishchettri112@gmail.com>
2022-07-26 20:56:39 +0530

    database setup and migration to mysql

--- date_and_time/date_and_time/settings.py
+++ date_and_time/date_and_time/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'date_and_time.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'date_and_time',
+        'USER': 'groot',
+        'PASSWORD': 'groot',
+        'PORT': 3306,
+        'HOST': '127.0.0.1'
     }
 }
 
